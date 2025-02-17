495fb2a4b7c105ec6fecfe688bb2f5ff55d15720
Asyraaf Razak <m.asyraaf.ra2ak@gmail.com>
2025-02-17 10:52:06 +0800

    add reqgistration and login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
+
+Route::redirect('/','/note')->name('dashboard');
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +17,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::middleware(['auth','verified'])->group(function() {
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+/*
+Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+Route::post('/note', [NoteController::class, 'store'])->name('note.store');
+Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
+Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
+*/
+
+Route::resource('note', NoteController::class);
+
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
